9d05b7a1ab64c22afcba66010672f8539c0c29e9
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2023-10-26 19:12:05 -0600

    Add SpectreConsole utils

---
+++ src/ARJE/Utils/Spectre/Console/Extensions/SelectionPromptExt.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using EnumsNET;
+using Spectre.Console;
+
+namespace ARJE.Utils.Spectre.Console.Extensions
+{
+    public static class SelectionPromptExt
+    {
+        public static SelectionPrompt<string> AddStyledChoices(
+            this SelectionPrompt<string> obj,
+            params (string Text, Style Style)[] choices)
+        {
+            foreach (var choice in choices)
+            {
+                string styledChoice = StyleUtils.Wrap(choice.Text, choice.Style);
+                obj.AddChoice(styledChoice);
+            }
+
+            return obj;
+        }
+
+        public static SelectionPrompt<TEnum> AddEnumChoices<TEnum>(this SelectionPrompt<TEnum> obj)
+            where TEnum : struct, Enum
+        {
+            IReadOnlyList<TEnum> choices = Enums.GetValues<TEnum>();
+            obj.AddChoices(choices);
+            return obj;
+        }
+
+        public static SelectionPrompt<T> UseStyledConverter<T>(
+            this SelectionPrompt<T> obj,
+            Func<T, Style> converter)
+            where T : notnull
+        {
+            obj.Converter = (choice) =>
+            {
+                string? choiceString = choice.ToString();
+                if (choiceString == null)
+                {
+                    return null!;
+                }
+
+                Style style = converter.Invoke(choice);
+                return StyleUtils.Wrap(choiceString, style);
+            };
+            return obj;
+        }
+    }
+}
---
+++ src/ARJE/Utils/Spectre/Console/StyleUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Spectre.Console;
+
+namespace ARJE.Utils.Spectre.Console
+{
+    public static class StyleUtils
+    {
+        public static string Wrap(string text, Color? foreground = null, Color? background = null, Decoration? decoration = null, string? link = null)
+        {
+            ArgumentNullException.ThrowIfNull(text);
+
+            var style = new Style(foreground, background, decoration, link);
+            return Wrap(text, style);
+        }
+
+        public static string Wrap(string text, Style style)
+        {
+            ArgumentNullException.ThrowIfNull(text);
+            ArgumentNullException.ThrowIfNull(style);
+
+            string markup = style.ToMarkup();
+            string escapedText = Markup.Escape(text);
+            return $"[{markup}]{escapedText}[/]";
+        }
+    }
+}
