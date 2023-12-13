28ddd0560c5288bab2e5af04540b5e486dbae328
CaballeroEscamilla <juanescamilla2026@gmail.com>
2023-12-13 12:51:10 -0600

    Panel para los metadatos

--- src/ARJE/SignPractice/Views/SelectView.axaml.cs
+++ src/ARJE/SignPractice/Views/SelectView.axaml.cs
@@ -1,17 +1,35 @@
 using System;
 using ARJE.Utils.Avalonia.MVC.Views;
+using Avalonia.Controls;
 using Avalonia.Interactivity;
+
 
 namespace ARJE.SignPractice.Views;
 
 public sealed partial class SelectView : ViewBase
 {
+
     public event Action? OnBackBtnClick;
 
-    protected override void OnInitializeComponent() => this.InitializeComponent();
+    public event Action? PointerEnter;
+
+    private Panel hiddenPanel;
+
+    protected override void OnInitializeComponent() {
+        this.InitializeComponent();
+         // Obtén una referencia al panel oculto por su nombre (definido en el archivo XAML)
+        //hiddenPanel = this.FindControl<Panel>("HiddenPanel");
+        // Inicialmente, oculta el panel
+        //hiddenPanel.IsVisible = true;
+    } 
 
     private void BackBtnClickHandler(object sender, RoutedEventArgs e)
     {
         this.OnBackBtnClick?.Invoke();
+    }
+
+    private void PointerEnterHandler(object sender, RoutedEventArgs e){
+        this.PointerEnter?.Invoke();
+        //hiddenPanel.IsVisible = true;
     }
 }
